982ee12978ce97672ad623ecd0c58febd4d6425c
eyyu <eva.yw.yu@gmail.com>
2016-10-13 12:17:55 -0700

    EY: Added to console printing / display function so that console can display items

--- src/gpsprint.cpp
+++ src/gpsprint.cpp
@@ -63,7 +63,7 @@ void printData(gpsmm& gpsData) {
 	short 	PRN; 		// DEBUG : storing vals if necessary -- to be simplified later
 	short 	elevation; 	// DEBUG : storing vals if necessary -- to be simplified later
 	short 	azimuth; 	// DEBUG : storing vals if necessary -- to be simplified late
-	bool 	used; 		// DEBUG : storing vals if necessary -- to be simplified later
+	bool 	isUsed; 		// DEBUG : storing vals if necessary -- to be simplified later
     
     //EY - 1 : May I ask, What is this one for?
     cout << "Current time: " << put_time(gmtime(&tim), "%T") << endl;
@@ -86,37 +86,40 @@ void printData(gpsmm& gpsData) {
 
     	if(gpsData.data()->fix.mode >1){ /* 2D or 3D */
 		    latitiude = gpsData.data()->fix.latitude;
-		    longitude =	gpsData.data()->fix.longitude;
-		 	
-		 	if(gpsData.data()->fix.mode > 2){ /* 3D only */
-		 		altitude  = gpsData.data()->fix.altitude;
-		 		climb = gpsData.data()->fix.climb;
-		 	}
-		 	else /*EY - 4 : IS THIS NECESSARY? */
-		 	{
-		 		//cout << "Not 3D Mode, cannot get altitude and climb";
-		 	}
-    	}
+            longitude = gpsData.data()->fix.longitude;
+    
+             cout   << timestamp << ":" 
+                    << " Longitude: " << longitude << ( longitude > 0 ? " N" : " S"); 
+                    << " Latitude: "  << latitude  << ( latitude  > 0 ? " E" : " W");
+                    << endl;
 
-		 for(int i = 0; i < gpsData.data()->satellites_visible; ++i )
-		 {
-			 	ssdB = gpsData.data()->skyview[i].ss;
-			 	PRN = gpsData.data()->skyview[i].PRN;
-			 	elevation = gpsData.data()->skyview[i].elevation;
-			 	azimuth = gpsData.data()->skyview[i].azimuth;
-			 	used = gpsData.data()->skyview[i].used;
-		 }
+            if(gpsData.data()->fix.mode > 2){ /* 3D only */
+                altitude  = gpsData.data()->fix.altitude;
+                climb = gpsData.data()->fix.climb;
+                
+                cout << "Altitude: " << altitude
+                     << "\tClimb: "  << climb
+                     << endl;
+            
+            }
+        }
+
+        for(int i = 0; i < gpsData.data()->satellites_visible; ++i )
+         {
+            ssdB       = gpsData.data()->skyview[i].ss;
+            PRN        = gpsData.data()->skyview[i].PRN;
+            elevation  = gpsData.data()->skyview[i].elevation;
+            azimuth    = gpsData.data()->skyview[i].azimuth;
+            isUsed     = gpsData.data()->skyview[i].used;
+        
+            cout << "PRN:"              << PRN
+                 << "\tElevatation: "   << elevation
+                 << "\tAzimuth: "       << azimuth
+                 << "\tSNR: "           << SNR
+                 << "\tUsed: "          << USED[isUsed];
+                 << "\n" << endl;  
+         }
 
     }
  
-
-    // Loop for following: 
-    // PRN
-    // Elevatation
-    // Azi
-    // SNR: 
-    // Used: 
 }
- /**
-  * NOTE TO SELF:  isNorthSOuthOnLongitute and isEastWestonLatitude Functions 
-  */
