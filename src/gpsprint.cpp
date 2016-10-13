ca4b00e5bbb0d7d2b17de3d4678f6c55e2c8f722
eyyu <eva.yw.yu@gmail.com>
2016-10-13 09:54:41 -0700

    EY: added the steps to get gps/sattelite values needed for display

--- src/gpsprint.cpp
+++ src/gpsprint.cpp
@@ -1,3 +1,23 @@
+/*------------------------------------------------------------------------------
+-- SOURCE FILE: gpsprint.cpp - print and display functions for gps program
+--
+-- PROGRAM: gpsprint
+--
+-- FUNCTIONS:
+-- void printData (gpsmm&)
+--
+--
+-- DATE: Oct. 12, 2016
+--
+-- REVISIONS: 
+-- v1   - JA
+-- v1.1 - EY
+--
+-- DESIGNER: EY / JA
+--
+--
+------------------------------------------------------------------------------*/
+
 #include "headers/consts.h"
 #include "headers/gpsprint.h"
 #include <iostream> 
@@ -7,8 +27,96 @@
 #include <iomanip> 
 
 using namespace std;
-
+/*--------------------------------------------------------------------------
+-- FUNCTION: printData
+--
+-- DATE: OCT. 12, 2016
+--
+-- REVISIONS: Version 1.1
+--
+-- DESIGNER: EY / JA
+--
+-- PROGRAMMER: v1.1 EY 
+-- v1.0 - JA
+--
+-- INTERFACE: void printData (gpsmm&)
+-- gpsData reference to a gpsmm object / instance
+--
+-- NOTES:
+-- v.1  design is simple, jsut to ensure basic functionality works 
+-- References: 
+-- https://fossies.org/dox/gpsd-3.16/gps_8h_source.html#l02010
+-- https://fossies.org/dox/gpsd-3.16/cgps_8c_source.html
+--------------------------------------------------------------------------*/
 void printData(gpsmm& gpsData) {
-    time_t tim = time(nullptr);    
+    
+    const char [] USED = {'N', 'Y'};
+    time_t tim = time(nullptr);  
+
+    double 	latititude; // DEBUG : storing vals if necessary -- to be simplified later
+    double 	longitude;  // DEBUG : storing vals if necessary -- to be simplified later
+    double 	altitude;   // DEBUG : storing vals if necessary -- to be simplified later
+    double 	climb;   	// DEBUG : storing vals if necessary -- to be simplified late
+    double 	timestamp;  // DEBUG : storing vals if necessary -- to be simplified later
+	
+	double  ssdB; 		// DEBUG : storing vals if necessary -- to be simplified later
+	short 	PRN; 		// DEBUG : storing vals if necessary -- to be simplified later
+	short 	elevation; 	// DEBUG : storing vals if necessary -- to be simplified later
+	short 	azimuth; 	// DEBUG : storing vals if necessary -- to be simplified late
+	bool 	used; 		// DEBUG : storing vals if necessary -- to be simplified later
+    
+    //EY - 1 : May I ask, What is this one for?
     cout << "Current time: " << put_time(gmtime(&tim), "%T") << endl;
+    
+    if (gpsData.data()->satellites_visible > 0)
+    {
+    	cerr << "No satellites visible." << endl; 
+    	return; //EY - 2: Erm.. is that all?
+    }
+
+
+    if(gpsData.data()->status == STATUS_NO_FIX /*0*/ )
+    {
+    	cout << "NO FIX." << endl;	
+    	return; //EY - 3: should I retry here? 
+    }
+    else 
+    {
+	    timestamp = gpsData.data()->skyview_time;
+
+    	if(gpsData.data()->fix.mode >1){ /* 2D or 3D */
+		    latitiude = gpsData.data()->fix.latitude;
+		    longitude =	gpsData.data()->fix.longitude;
+		 	
+		 	if(gpsData.data()->fix.mode > 2){ /* 3D only */
+		 		altitude  = gpsData.data()->fix.altitude;
+		 		climb = gpsData.data()->fix.climb;
+		 	}
+		 	else /*EY - 4 : IS THIS NECESSARY? */
+		 	{
+		 		//cout << "Not 3D Mode, cannot get altitude and climb";
+		 	}
+    	}
+
+		 for(int i = 0; i < gpsData.data()->satellites_visible; ++i )
+		 {
+			 	ssdB = gpsData.data()->skyview[i].ss;
+			 	PRN = gpsData.data()->skyview[i].PRN;
+			 	elevation = gpsData.data()->skyview[i].elevation;
+			 	azimuth = gpsData.data()->skyview[i].azimuth;
+			 	used = gpsData.data()->skyview[i].used;
+		 }
+
+    }
+ 
+
+    // Loop for following: 
+    // PRN
+    // Elevatation
+    // Azi
+    // SNR: 
+    // Used: 
 }
+ /**
+  * NOTE TO SELF:  isNorthSOuthOnLongitute and isEastWestonLatitude Functions 
+  */
