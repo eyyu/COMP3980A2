6ef6837032e8a0fb2efc526dcf62c02403efcc95
eyyu <eva.yw.yu@gmail.com>
2016-10-14 17:29:47 -0700

    EY: fix for macro definition

--- src/gpsprint.cpp
+++ src/gpsprint.cpp
@@ -26,6 +26,21 @@
 #include <ctime> 
 #include <iomanip> 
 
+// DEFINE
+#define DEFINE_MEMBER_CHECKER(member) \
+    template<typename T, typename V = bool> \
+    struct has_ ## member : false_type { }; \
+    template<typename T> \
+    struct has_ ## member<T, \
+        typename enable_if< \
+            !is_same<decltype(declval<T>().member), void>::value, \
+            bool \
+            >::type \
+        > : true_type { };
+
+#define HAS_MEMBER(C, member) \
+    has_ ## member<C>::value
+    
 DEFINE_MEMBER_CHECKER(skyview)
 
 using namespace std;
--- src/headers/gpsprint.h
+++ src/headers/gpsprint.h
@@ -39,18 +39,5 @@ void printToConsole(gps_data_t *gpsData);
 
 #endif
 
-// DEFINE
-#define DEFINE_MEMBER_CHECKER(member) \
-    template<typename T, typename V = bool> \
-    struct has_ ## member : false_type { }; \
-    template<typename T> \
-    struct has_ ## member<T, \
-        typename enable_if< \
-            !is_same<decltype(declval<T>().member), void>::value, \
-            bool \
-            >::type \
-        > : true_type { };
 
-#define HAS_MEMBER(C, member) \
-    has_ ## member<C>::value
 
