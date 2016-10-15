3f8cd96b6ff607813c1445acaed4a14c2e5a1f09
eyyu <eva.yw.yu@gmail.com>
2016-10-15 10:13:54 -0700

    EY: Changes for minimal pi version

--- src/gpsprint.cpp
+++ src/gpsprint.cpp
@@ -56,29 +56,16 @@ using namespace std;
 --------------------------------------------------------------------------*/
 
 void printData(struct gps_data_t * gpsData) {
-    //JA - Use enum class instead of const char array - Provides more meaning than arbitrary N and Y
-    const char USED[2] {'N', 'Y'};
-    time_t tim = time(nullptr);
-
-    //JA - If you need to store values for repeated use, define them with the smallest necessary scope
-    double  latitude;   // DEBUG : storing vals if necessary -- to be simplified later
-    double  longitude;  // DEBUG : storing vals if necessary -- to be simplified later
-    double  timestamp;  // DEBUG : storing vals if necessary -- to be simplified later
-  
-    double  ssdB;     // DEBUG : storing vals if necessary -- to be simplified later
-    short   PRN;    // DEBUG : storing vals if necessary -- to be simplified later
-    short   elevation;  // DEBUG : storing vals if necessary -- to be simplified later
-    short   azimuth;  // DEBUG : storing vals if necessary -- to be simplified late
-    bool  isUsed;   // DEBUG : storing vals if necessary -- to be simplified later
-    
-    //EY - 1 : May I ask, What is this one for?
     //JA This is to print out the current time is UTC. Don't know if we should use this vs the gps fix time, so I put this one here
     //EY - 1: Ah... I believe tihs is actually using the satellite time (we will probably need to parse it  from epoch time) 
+    //EY: NOTE ! put_time is not a decalred function in pi.... not sure why :S
+    
+    /*time_t tim = time(nullptr);
     cout << "Current time: " << put_time(gmtime(&tim), "%T") << endl;
+    */
     
     if (gpsData->satellites_visible < 0)
     {
-      // cerr << "No satellites were visible." << endl; 
       return; 
     }
 
@@ -94,6 +81,9 @@ void printData(struct gps_data_t * gpsData) {
       timestamp = gpsData->skyview_time;
 
       if (gpsData->fix.mode > 1){ /* 2D or 3D */
+      double  latitude;   // DEBUG : storing vals if necessary -- to be simplified later
+      double  longitude;  // DEBUG : storing vals if necessary -- to be simplified later
+      double  timestamp;  // DEBUG : storing vals if necessary -- to be simplified later
        
             latitude = gpsData->fix.latitude;
             longitude = gpsData->fix.longitude;
@@ -112,6 +102,12 @@ void printData(struct gps_data_t * gpsData) {
 
         for(int i = 0; i < gpsData->satellites_visible; ++i)
         {
+            double  ssdB;     // DEBUG : storing vals if necessary -- to be simplified later
+            short   PRN;    // DEBUG : storing vals if necessary -- to be simplified later
+            short   elevation;  // DEBUG : storing vals if necessary -- to be simplified later
+            short   azimuth;  // DEBUG : storing vals if necessary -- to be simplified late
+            bool  isUsed;   // DEBUG : storing vals if necessary -- to be simplified later
+            
             ssdB       = gpsData->ss[i];
             PRN        = gpsData->PRN[i];
             elevation  = gpsData->elevation[i];
@@ -122,8 +118,8 @@ void printData(struct gps_data_t * gpsData) {
                  << "\tElevatation: "   << elevation
                  << "\tAzimuth: "       << azimuth
                  << "\tSNR: "           << ssdB
-                 << "\tUsed: "          << (isUsed? 'Y' : 'N');
-                 << endl;  
+                 << "\tUsed: "          << (isUsed? 'Y' : 'N')
+                   << endl;  
         }
         cout << endl;
     }
