d1653b7f1e4afd95eaff02b78eeebc39b1d79369
eyyu <eva.yw.yu@gmail.com>
2016-10-15 10:00:31 -0700

    EY: reverted to version 30b804bd7992fa6a630033d6eaf8cfe5ba358c21

--- src/gpsprint.cpp
+++ src/gpsprint.cpp
@@ -52,7 +52,6 @@ using namespace std;
 -- ISSUE:
 - r pi runs an older version of the api , and "skyview " variable is not found 
 - proposed solution reference for cross - version compatibility: http://stackoverflow.com/questions/1005476/how-to-detect-whether-there-is-a-specific-member-variable-in-class  
-- additonal issues : compiler issue for undeclared data -- proposed solution -- preprocessor directives 
 --------------------------------------------------------------------------*/
 void printData(struct gps_data_t * gpsData) {
     //JA - Use enum class instead of const char array - Provides more meaning than arbitrary N and Y
@@ -60,9 +59,15 @@ void printData(struct gps_data_t * gpsData) {
     time_t tim = time(nullptr);
 
     //JA - If you need to store values for repeated use, define them with the smallest necessary scope
-    double 	latitude;   // DEBUG : storing vals if necessary -- to be simplified later
-    double 	longitude;  // DEBUG : storing vals if necessary -- to be simplified later
-    double 	timestamp;  // DEBUG : storing vals if necessary -- to be simplified later
+    double  latitude;   // DEBUG : storing vals if necessary -- to be simplified later
+    double  longitude;  // DEBUG : storing vals if necessary -- to be simplified later
+    double  timestamp;  // DEBUG : storing vals if necessary -- to be simplified later
+  
+    double  ssdB;     // DEBUG : storing vals if necessary -- to be simplified later
+    short   PRN;    // DEBUG : storing vals if necessary -- to be simplified later
+    short   elevation;  // DEBUG : storing vals if necessary -- to be simplified later
+    short   azimuth;  // DEBUG : storing vals if necessary -- to be simplified late
+    bool  isUsed;   // DEBUG : storing vals if necessary -- to be simplified later
     
     //EY - 1 : May I ask, What is this one for?
     //JA This is to print out the current time is UTC. Don't know if we should use this vs the gps fix time, so I put this one here
@@ -71,23 +76,23 @@ void printData(struct gps_data_t * gpsData) {
     
     if (gpsData->satellites_visible < 0)
     {
-    	// cerr << "No satellites were visible." << endl; 
-    	return; 
+      // cerr << "No satellites were visible." << endl; 
+      return; 
     }
 
     if(gpsData->status == STATUS_NO_FIX /*0*/ )
     {
-    	cout << "NO FIX." << endl;	
-    	return; //EY - 3: should I retry here? JA - Nope, Aman showed a screen of what a failure to get a fix looked like. We should porobably do that
+      cout << "NO FIX." << endl;  
+      return; //EY - 3: should I retry here? JA - Nope, Aman showed a screen of what a failure to get a fix looked like. We should porobably do that
     }
     
     else 
     
     {
-	    timestamp = gpsData->skyview_time;
+      timestamp = gpsData->skyview_time;
 
-    	if (gpsData->fix.mode > 1){ /* 2D or 3D */
-		   
+      if (gpsData->fix.mode > 1){ /* 2D or 3D */
+       
             latitude = gpsData->fix.latitude;
             longitude = gpsData->fix.longitude;
     
@@ -105,25 +110,28 @@ void printData(struct gps_data_t * gpsData) {
 
         for(int i = 0; i < gpsData->satellites_visible; ++i)
         {
-                printSatelliteDataHelper(gpsData->PRN[i],
-                                         gpsData->elevation[i],
-                                         gpsData->azimuth[i],
-                                         gpsData->ss[i],
-                                         gpsData->used[i]);
+            if(hasSkyview<gps_data_t>){
+                // ssdB       = gpsData->skyview[i].ss;
+                // PRN        = gpsData->skyview[i].PRN;
+                // elevation  = gpsData->skyview[i].elevation;
+                // azimuth    = gpsData->skyview[i].azimuth;
+                // isUsed     = gpsData->skyview[i].used;
+            }
+            else
+            {
+                ssdB       = gpsData->ss[i];
+                PRN        = gpsData->PRN[i];
+                elevation  = gpsData->elevation[i];
+                azimuth    = gpsData->azimuth[i];
+                isUsed     = gpsData->used[i];
+            }
+
+            cout << "PRN:"              << PRN
+                 << "\tElevatation: "   << elevation
+                 << "\tAzimuth: "       << azimuth
+                 << "\tSNR: "           << ssdB
+                 << "\tUsed: "          << (isUsed? 'Y' : 'N');
+                 << endl << endl;  
         }
     }
 }
-
-void printSatelliteDataHelper(short PRN,
-                          short elevation,
-                          short azimuth, 
-                          double ssdB, 
-                          bool isUsed)
-{
-    cout << "PRN:"              << PRN
-         << "\tElevatation: "   << elevation
-         << "\tAzimuth: "       << azimuth
-         << "\tSNR: "           << ssdB
-         << "\tUsed: "          << (isUsed? 'Y' : 'N')
-         << endl << endl;    
-} 
--- src/headers/gpsprint.h
+++ src/headers/gpsprint.h
@@ -6,22 +6,56 @@
 
 void printData(gps_data_t *gpsData);
 void printToConsole(gps_data_t *gpsData);
-void printSatelliteDataHelper(short PRN,
-                         		short elevation,
-                          		short azimuth, 
-                          		double ssdB, 
-                          		bool isUsed)
 
-// template < typename T, typename = void > 
-// struct hasSkyview 
-// 	: std::false_type { };
+/*--------------------------------------------------------------------------
+-- FUNCTION TEMPLATE: hasSkyview
+--
+-- DATE: OCT. 14, 2016
+--
+-- REVISIONS: Set Version 2.0
+--
+-- DESIGNER: Eva Yu
+--
+-- PROGRAMMER: Eva Yu
+--
+-- INTERFACE: [returntype] [fucntionName] (functionParams)
+-- [functionParam0] [description]
+-- [functionParam1] [description]
+-- [functionParam2] [description]
+--
+-- RETURNS: 
+--
+-- NOTES:
+-- 
+--------------------------------------------------------------------------*/
+template < typename T, typename = void > 
+struct hasSkyview 
+	: std::false_type { };
 
+/*--------------------------------------------------------------------------
+-- FUNCTION TEMPLATE: hasSkyview
+--
+-- DATE: OCT. 14, 2016
+--
+-- REVISIONS: Set Version 2.0
+--
+-- DESIGNER: Eva Yu
+--
+-- PROGRAMMER: Eva Yu
+--
+-- INTERFACE: [returntype] [fucntionName] (functionParams)
+-- [functionParam0] [description]
+-- [functionParam1] [description]
+-- [functionParam2] [description]
+--
+-- RETURNS: 
+--
+-- NOTES:
+-- 
+--------------------------------------------------------------------------*/
 
-// template < typename T >
-// struct hasSkyview < T, decltype(std::declval<T>().skyview, void() )> 
-// 	: std::true_type  { };
+template < typename T >
+struct hasSkyview < T, decltype(std::declval<T>.skyview, void() )> 
+	: std::true_type  { };
 
 #endif
-
-
-
