d01467a327eb0460da82d80e28177bc8d84aa192
John Agapeyev <jagapeyev@gmail.com>
2016-10-15 21:27:22 -0700

    Removed c++ thread that prevented code from running for unknown reason

--- src/gps-utils.cpp
+++ src/gps-utils.cpp
@@ -10,24 +10,13 @@ using namespace std;
 
 void startReading(gpsmm& gpsData) 
 {
-    thread readThread(readLoop, gpsData);
-    readThread.join();
-}
-
-/*
- * Left incomplete atm because I'm not sure how to check if read fails with gpsmm
- * Documentation suggests that it returns null on failure, but it might also return void
- * No obvious last error method to call either
- */
-void readLoop(gpsmm gpsData) 
-{
     struct gps_data_t *newData;
     for (;;) 
     {
         if (!gpsData.waiting(50000000)) {
             continue;
         }
-        if ((newData = gpsData.read()) == NULL) 
+        if ((newData = gpsData.read()) == nullptr) 
         {
             //Failure
         } else {
